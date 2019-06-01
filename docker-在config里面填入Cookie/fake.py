e936ccd095e260aab75d3ce09f65a3cf44599622
MacsedProtoss <yu410420@gmail.com>
2019-06-01 15:28:46 +0800

    v2.0.0
    1.修复部分组件版本过低引起的安全警告
    2.细化py与docker，并且给出更加人性化的readme

---
+++ docker-在config里面填入Cookie/fake.py
@@ -0,0 +1,23 @@
+import requests
+import time
+from config import Cookie
+
+url="http://202.114.27.5/login/hponlinetime.php"
+
+Cookies=Cookie
+
+print (Cookies)
+
+payload=[{'mesg': 'one'},{'mesg':'two'}]
+headers={"Host":"202.114.27.5","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Linux; Android Q; Brand PhoneModel seriesNumber)","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3","Referer":"http://202.114.27.5/login/myhome.php","Accept-Encoding":"gzip, deflate","Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7","Cookie":Cookies,"Connection":"keep-alive"}
+s = requests.Session()
+r = s.get(url, headers=headers)
+
+print (r.status_code)
+
+
+while True:
+    time.sleep(1)
+    r = s.get(url, headers=headers)
+    print (r.status_code)
+
